65779e80aeb2293f4693dad0ccb7a51ef86d9341
Chris Boling <manwithnoshoes@gmail.com>
2013-05-29 17:47:30 -0500

    PID Version 0.1

---
+++ Assets/Standard Assets/Scripts/Utility Scripts/PIDController.js
@@ -0,0 +1,16 @@
+#pragma strict
+ var rightPos : double; // this is where we want it to go.
+ var realPos : double; // this is where it is going.
+ var time : double; // the present time
+ var proGain : double; // Proportional gain
+ var intGain : double; // Integral gain
+ var derGain : double; // Derivative gain
+ var manipVar : double; // The manipulated variable; what we are looking for.
+ 
+function Start () {
+
+}
+
+function Update () {
+
+}
